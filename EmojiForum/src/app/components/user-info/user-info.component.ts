9718e9564d1c2638f2394d94992515d0de7705e2
RaikkoP <71177699+RaikkoP@users.noreply.github.com>
2024-01-04 15:09:52 +0200

    Seperated Dashboard into two pieces

--- EmojiForum/src/app/components/user-info/user-info.component.ts
+++ EmojiForum/src/app/components/user-info/user-info.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import UserResponse from '../../../model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-info',
@@ -7,6 +10,47 @@ import { Component } from '@angular/core';
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.scss'
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
 
+  id: Number = 0;
+  username: String = '';
+  profilePic: String = '';
+  likes: Number = 0;
+  dislikes: Number = 0;
+  posts: Number = 0;
+  emojis: any[] = [];
+  selectedEmoji: String = '😀'
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { };
+
+  getEmojis() {
+    return this.http.get<any>('https://emoji-api.com/emojis?access_key=4f7510922dd61022149ba2821e2f66f3bb63685c')
+      .subscribe(data => {
+        this.emojis = data;
+      });
+  }
+  switchSelected(selected: String) {
+    this.selectedEmoji = selected;
+  }
+
+  getAuthentication() {
+    return this.http.get<UserResponse>('http://localhost:3000/user/get/', { withCredentials: true }).subscribe({
+      next: res => {
+        this.username = res.username;
+        this.profilePic = res.profilePic;
+        this.id = res.id;
+      },
+      error: err => {
+        this.router.navigate(['/login']);
+      }
+    })
+  }
+
+  ngOnInit() {
+    this.getAuthentication();
+    this.getEmojis();
+  }
 }
